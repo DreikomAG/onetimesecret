8b55683748c1c0aed75233cda1447f3b457f4381
Raphael Büchi <raphael.buechi@hotmail.com>
2020-09-07 21:25:15 +0200

    Fixed presistent

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,8 @@
 # set OTS secret if supplied by ENV or if default
 
 # start Redis
-/etc/init.d/redis-server start
+#/etc/init.d/redis-server start
+redis-server /etc/onetime/redis.conf
 
 # start OTS
 cd /var/lib/onetime && bundle exec thin -e dev -R config.ru -p 7143 start
